ee6df5c98cf0303dcd4fbeec0fe4e15f070fd9b0
kuaker <sbumission@gmail.com>
2024-06-26 12:01:04 -0300

    first commit

---
+++ cypress/e2e/tests/login/register-test.spec.ts
@@ -0,0 +1,42 @@
+import AssertAccountCreatedPage from "../../../support/assertPages/AssertAccountCreatedPage"
+import AssertDeleteAccount from "../../../support/assertPages/AssertDeleteAccount"
+import AssertHeader from "../../../support/assertPages/AssertHeader"
+import AssertHomePage from "../../../support/assertPages/AssertHomePage"
+import AssertLoginPage from "../../../support/assertPages/AssertLoginPage"
+import AssertSignUpPage from "../../../support/assertPages/AssertSignUpPage"
+import AccountInformation from "../../../support/modules/AccountInformation"
+import AddressInformation from "../../../support/modules/AddressInformation"
+import Header from "../../../support/modules/Header"
+import AccountCreatedPage from "../../../support/pages/AccountCreatedPage"
+import LoginPage from "../../../support/pages/LoginPage"
+import SignUpPage from "../../../support/pages/SignUpPage"
+
+context('E2E Tests', () => {
+
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    describe('Register test cases', () => {
+        it('Register User', () => {
+            AssertHomePage.checkSliderIsVisible()
+            Header.clickOnLoginSignUpButton()
+            AssertLoginPage.checkSignUpTitleIsVisible()
+            LoginPage.fillNameAndEmailForSignUp()
+            LoginPage.clickOnSignUpButton()
+            AssertSignUpPage.checkAccountInformationTitleIsVisible()
+            AccountInformation.setNameEmailAndPassword()
+            AccountInformation.setDateOfBirth()
+            AccountInformation.selectCheckBoxNewLetter().check()
+            AccountInformation.selectCheckBoxOfferss().check()
+            AddressInformation.fillFormWithInformation()
+            SignUpPage.clickOnCreateAccountButton()
+            AssertAccountCreatedPage.checkAccountWasCreated()
+            AccountCreatedPage.clickOnContinueButton()
+            AssertHeader.checkLoggedAsUsernameIsVisible()
+            Header.clickOnDeleteAccount()
+            AssertDeleteAccount.checkAccountDeleteMessagesAreVisible()
+        })
+    })
+
+})
